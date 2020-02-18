ee5c753459ca3846bf3c9e9d45da4e7117255f63
Roman Karas <roman7722@yandex.ru>
2020-02-18 12:44:02 +0300

    Deleted old files

--- src/token/token.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { Resolver } from '@nestjs/graphql';
-import { TokenService } from './token.service';
-
-@Resolver()
-export class TokenResolver {
-  constructor(private readonly tokenService: TokenService) {}
-}
